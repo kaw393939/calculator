c6aae062f15d009fc8382ffe28b277fd5531400c
Keith Williams <keith@webizly.com>
2019-10-15 17:13:08 -0400

    Feature: Created Calculator Class and of Results

--- src/Calculator.py
+++ src/Calculator.py
@@ -1,5 +1,14 @@
+def addition(a, b):
+    c = a + b
+    return c
+
+
 class Calculator:
     result = 0
 
     def __init__(self):
         pass
+
+    def add(self,a,b):
+        self.result = a + b
+        return self.result
--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -7,6 +7,15 @@ class MyTestCase(unittest.TestCase):
         calculator = Calculator()
         self.assertIsInstance(calculator, Calculator)
 
+    def test_addition(self):
+        calculator = Calculator()
+        self.assertEqual(calculator.add(1,1), 2)
+
+    def test_results_property(self):
+        calculator = Calculator()
+        calculator.add(1, 1)
+        self.assertEqual(calculator.result, 2)
+
 
 if __name__ == '__main__':
     unittest.main()
