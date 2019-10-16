45c4c3b289e9fdddd9afba6c7d44cb79048169c7
Keith Williams <keith@webizly.com>
2019-10-16 17:51:01 -0400

    Fix: Updated using the static method

--- src/Calculator.py
+++ src/Calculator.py
@@ -3,12 +3,21 @@ def addition(a, b):
     return c
 
 
+def subtraction(a, b):
+    c = a - b
+    return c
+
+
 class Calculator:
     result = 0
 
     def __init__(self):
         pass
 
-    def add(self,a,b):
-        self.result = a + b
+    def add(self, a, b):
+        self.result = addition(a, b)
+        return self.result
+
+    def subtract(self, a, b):
+        self.result = subtraction(a, b)
         return self.result
--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -9,12 +9,16 @@ class MyTestCase(unittest.TestCase):
 
     def test_addition(self):
         calculator = Calculator()
-        self.assertEqual(calculator.add(1,1), 2)
+        self.assertEqual(calculator.add(1, 1), 2)
+
+    def test_subtraction(self):
+        calculator = Calculator()
+        self.assertEqual(calculator.subtract(1, 1), 0)
 
     def test_results_property(self):
         calculator = Calculator()
-        calculator.add(1, 1)
-        self.assertEqual(calculator.result, 2)
+        calculator.add(2, 1)
+        self.assertEqual(calculator.result, 3)
 
 
 if __name__ == '__main__':
