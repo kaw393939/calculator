9489306d3aaf8741b923b661d1219651e35eba4e
Keith Williams <keith@webizly.com>
2019-10-16 20:58:49 -0400

    Fix: added an assertion to check results on each arithmatic operation

--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -11,12 +11,17 @@ class MyTestCase(unittest.TestCase):
 
     def test_addition(self):
         self.assertEqual(self.calculator.add(1, 1), 2)
+        self.assertEqual(self.calculator.result, 2)
+
 
     def test_subtraction(self):
         self.assertEqual(self.calculator.subtract(1, 1), 0)
+        self.assertEqual(self.calculator.result, 0)
+
 
     def test_results_property(self):
         self.assertEqual(self.calculator.result, 0)
+
 
 
 if __name__ == '__main__':
