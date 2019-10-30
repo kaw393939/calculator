0d8ca390830109fc422cb069341a82e879352426
Keith Williams <keith@webizly.com>
2019-10-29 20:12:54 -0400

    Feature: Refactor for Travis Testing and running multiple tests

--- CsvReader/CsvReader.py
+++ CsvReader/CsvReader.py
@@ -14,6 +14,7 @@ class CsvReader:
         self.data = []
         relative = Path(filepath)
         absolute = relative.absolute()
+        print(absolute)
         with open(absolute) as text_data:
             csv_data = csv.DictReader(text_data, delimiter=',')
             for row in csv_data:
