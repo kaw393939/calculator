1a5b8bbec6acf23a99b35e3f33f30ab60e2f2087
Keith Williams <keith@webizly.com>
2019-10-29 20:17:47 -0400

    Feature: Refactor for Travis Testing and running multiple tests

--- Tests/test_CSVTests.py
+++ Tests/test_CSVTests.py
@@ -6,7 +6,7 @@ from CsvReader.CsvReader import CsvReader, ClassFactory
 class MyTestCase(unittest.TestCase):
 
     def setUp(self) -> None:
-        self.csv_reader = CsvReader(os.path.join(os.path.dirname(__file__), '/Tests/Data/employee_birthday.csv'))
+        self.csv_reader = CsvReader('Tests/Data/employee_birthday.csv')
 
     def test_return_data_as_objects(self):
         people = self.csv_reader.return_data_as_objects('person')
