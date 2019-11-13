7d7c658b4336681b5074f03b3d2c2a656273aa49
Keith Williams <keith@webizly.com>
2019-11-12 22:02:15 -0500

    `Task: got sqlite to work for toolbox and desktop

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM python:3.7
 
 COPY . /web
+WORKDIR /web
+RUN pip install -r ./requirements.txt
 ENTRYPOINT ["python"]
 CMD ["/web/Database/sqlite_create.py"]
--- Statistics/Getsample.py
+++ Statistics/Getsample.py
@@ -1,6 +1,5 @@
 from random import random
 
-
 def getSample(data, sample_size):
     random_values = random.sample(data, k=sample_size)
     return random_values
